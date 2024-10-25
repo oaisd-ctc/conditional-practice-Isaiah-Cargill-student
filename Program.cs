bd1f8138b537ae79a95688273bb9becb0dd793db
Isaiah-Cargill-student <isca71b@student.oaisd.org>
2024-10-25 14:24:43 +0000

    adding days check vowel  positive and negative

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 
+using System.Globalization;
+
 public class Program
 {
     public static void Main(string[] args)
@@ -6,14 +8,105 @@ public class Program
         FahrenheitToCelsius(68); // Test FahrenheitToCelsius Function with a random value
 
         // Test your functions by calling them below:
-        // CheckForPositiveNegativeZero(-1); // This should output: "Your number is negative"
-        // CheckForPositiveNegativeZero(0); // This should output: "Your number is zero"
-        // CheckForPositiveNegativeZero(1); // This should output: "Your number is positive"
+         CheckForPositiveNegativeZero(-1); // This should output: "Your number is negative"
+         CheckForPositiveNegativeZero(0); // This should output: "Your number is zero"
+         CheckForPositiveNegativeZero(1); // This should output: "Your number is positive"
         // ...
         // ...
         // DisplayDayOfWeek(0); // This should output: "Thursday"
 
     }
+public static void CheckForPositiveNegativeZero(int number)
+{
+if (number < 0)
+{
+ Console.WriteLine(" your number is negative");
+}
+
+else if (number > 0) 
+{
+    Console.WriteLine(" your number is positive");
+}
+
+else if (number == 0)
+{
+    Console.WriteLine(" your number is zero");
+}
+
+}
+public static void FindMinimum(int num1, int num2, int num3)
+{
+    if (num1 < num2 && num1 < num3 )
+    {
+      Console.WriteLine(" The minimum value is" + num1);
+    }
+
+    else if (num2 < num1 && num2 < num3)
+    {
+        Console.WriteLine(" The minimum value is" + num2);
+    }
+
+    else if (num3 <num2 && num3 < num1)
+    {
+         Console.WriteLine(" The minimum value is" + num3);
+    }
+}
+
+public static void FindMaximum(int num1, int num2, int num3)
+{
+    if (num1 > num2 && num1 > num3)
+    {
+        Console.WriteLine(" the maximum value is" + num1);
+    }
+    
+    else if (num2 > num1 && num2 > num3)
+    {
+         Console.WriteLine(" the maximum value is" + num2);
+    }
+    else if (num3 > num2 && num3 > num1)
+    {
+        Console.WriteLine( " the maximum value is" + num3);
+    }
+}  
+ public static void IsDivisibleBy5(int number)
+ {
+    if(number % 5==0)
+    {
+ Console.WriteLine(number + " is not divisible by 5");
+    }
+
+    else
+    {
+     Console.WriteLine(number + " is divisible by 5");
+    }
+ }
+
+ public static void CheckEvenOrOdd(int number)
+ {
+    if(number % 2==1)
+    {
+     Console.WriteLine(number + " is an odd number");
+    }
+
+    else if(number % 2==0)
+    {
+        Console.WriteLine(number + " is an even number");
+    }
+ }
+
+  public static void CheckVowelOrConsonant(char letter)
+  {
+    if (letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u' || letter == 'A' || letter == 'E' || letter == 'I' || letter == 'O' || letter == 'U')
+    {
+      Console.WriteLine(letter + " is a vowel.");
+    }
+
+    else{
+        Console.WriteLine(letter + " is a consonant");
+    }
+   
+  }
+    
 
     public static void FahrenheitToCelsius(double fahrenheit)
     {
@@ -23,4 +116,49 @@ public class Program
 
     // Define your functions below:
 
+    public static void DisplayDayOfWeek(int dayCode) 
+    {
+        if(dayCode == 0)
+        {
+            Console.WriteLine(dayCode + " - sunday");
+        }
+        
+        else if(dayCode == 1)
+        {
+            Console.WriteLine(dayCode + " - monday");
+        }
+          
+          else if(dayCode == 2)
+        {
+            Console.WriteLine(dayCode + " - tuesday");
+        }
+
+        else if(dayCode == 3)
+        {
+            Console.WriteLine(dayCode + " - wednesday");
+        }
+
+        else if(dayCode == 4)
+        {
+            Console.WriteLine(dayCode + " - thursday");
+        }
+
+          else if(dayCode == 5)
+        {
+            Console.WriteLine(dayCode + " - friday");
+        }
+          
+          else if(dayCode == 6)
+        {
+            Console.WriteLine(dayCode + " - saturday");
+        }
+          
+          else 
+        {
+            Console.WriteLine(dayCode + " - invalid");
+        }
+    }
+      
+    
+    
 }
